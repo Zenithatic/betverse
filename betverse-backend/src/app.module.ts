21bf090b467ae75fd2e67fee414a5f35ad92f7fa
Patrick <patmc55555@gmail.com>
2025-06-13 23:11:24 -0400

    forgot to save files

--- betverse-backend/src/app.module.ts
+++ betverse-backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { HealthController } from './health/health.controller';
 import { HealthService } from './health/health.service';
 
@@ -7,14 +6,5 @@ import { HealthService } from './health/health.service';
   imports: [],
   controllers: [HealthController],
   providers: [HealthService],
-=======
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> main
 })
 export class AppModule {}
